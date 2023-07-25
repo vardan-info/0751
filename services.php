074b998dfdad982d670d056f0d8ed43e5f38cd0d
vardan <69822867+vardan-info@users.noreply.github.com>
2023-07-26 03:31:46 +0400

    Update and rename services.html to services.php

--- services.php
+++ services.php
@@ -194,6 +194,81 @@ Enter Message:	<textarea name="message"></textarea>
 
 <input type="submit" value="Send Form">
 </form>
+   <?php
+  $name = $_POST['name'];
+  $visitor_email = $_POST['email'];
+  $message = $_POST['message'];
+?>
+   <?php
+	$email_from = 'yourname@yourwebsite.com';
+
+	$email_subject = "New Form submission";
+
+	$email_body = "You have received a new message from the user $name.\n".
+                            "Here is the message:\n $message".
+?>
+   <?php
+
+  $to = "yourname@yourwebsite.com";
+
+  $headers = "From: $email_from \r\n";
+
+  $headers .= "Reply-To: $visitor_email \r\n";
+
+  mail($to,$email_subject,$email_body,$headers);
+
+ ?>
+   
+<?php
+  $to = "name1@website-name.com, name2@website-name.com,name3@website-name.com";
+
+  mail($to,$email_subject,$email_body,$headers);
+?>
+   
+<?php
+$to = "name1@website-name.com, name2@website-name.com,name3@website-name.com";
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply-To: $visitor_email \r\n";
+
+$headers .= "Cc: someone@domain.com \r\n";
+
+$headers .= "Bcc: someoneelse@domain.com \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+?>
+   <?php
+function IsInjected($str)
+{
+    $injections = array('(\n+)',
+           '(\r+)',
+           '(\t+)',
+           '(%0A+)',
+           '(%0D+)',
+           '(%08+)',
+           '(%09+)'
+           );
+               
+    $inject = join('|', $injections);
+    $inject = "/$inject/i";
+    
+    if(preg_match($inject,$str))
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+}
+
+if(IsInjected($visitor_email))
+{
+    echo "Bad email value!";
+    exit;
+}
+?>
    </body> 
 </html>
 
